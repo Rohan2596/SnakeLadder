c59321c2f254d0056f63514cb1069df6b0525929
Rohan2596 <rohankadam662@gmail.com>
2019-11-15 12:43:07 +0530

    Calculating Won Count & Loss Position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -8,18 +8,24 @@ echo "Welcome to Snake And Ladder"
 PLAYER=1;
 START_POSITION=0;
 END_POSITION=100;
-
-while [ $START_POSITION -lt $END_POSITION ] && [ $START_POSITION -ge 0 ]
+WON_COUNT=0
+delcare -A dieChart
+function dice(){
+while [ $START_POSITION -le $END_POSITION ] && [ $START_POSITION -ge 0 ]
 do
 rollDice=$(( RANDOM%6+1 ));
 checkPosition=$(( RANDOM%3 ));
 
 if [[ $checkPosition -eq 0   ]]
 then
+
 	  START_POSITION=$(( $START_POSITION + $rollDice ))
+	WON_COUNT=$(( $WON_COUNT+1  ))
 elif [ $checkPosition -eq 1 ] && [ $START_POSITION -ge 0 ]
 then
    	START_POSITION=$(( $START_POSITION - $rollDice ))
+
+dieChart["Position"$checkPosition]=$(( $START_POSITION ))
  if [[ $START_POSITION -le 0 ]]
  then
  START_POSITION=0
@@ -28,3 +34,8 @@ else
    	START_POSITION=$START_POSITION
 fi
 done
+
+}
+dice
+echo ${dieChart[@]} 
+echo $WON_COUNT
