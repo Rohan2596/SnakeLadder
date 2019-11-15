31b19bde22a338c332b1d7592cc26d8fb4e5cd2d
Rohan2596 <rohankadam662@gmail.com>
2019-11-15 18:31:41 +0530

    two player Condition

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,37 +5,62 @@
 
 echo "Welcome to Snake And Ladder"
 
-PLAYER=1;
+
 START_POSITION=0;
-END_POSITION=100;
-WON_COUNT=0
-delcare -A dieChart
+END_POSITION=20;
+WON_COUNT=0;
+COUNT=0;
+declare -A dieChart
+declare -A playersChart
 function dice(){
-while [ $START_POSITION -le $END_POSITION ] && [ $START_POSITION -ge 0 ]
+PLAYER=2;
+for(( i=0;i<=$PLAYER;i++  ))
 do
-rollDice=$(( RANDOM%6+1 ));
-checkPosition=$(( RANDOM%3 ));
+echo "+++++++++++++++++++++++++++++++++++++++"
+WON_COUNT=0;
+START_POSITION=0;
+	while [ $START_POSITION -lt $END_POSITION ] && [ $START_POSITION -ge 0 ]
+	do
+     rollDice=$(( RANDOM%6+1 ));
+     checkPosition=$(( RANDOM%3 )); 
 
-if [[ $checkPosition -eq 0   ]]
-then
+		if [[ $checkPosition -eq 0   ]]
+		then
 
-	  START_POSITION=$(( $START_POSITION + $rollDice ))
-	WON_COUNT=$(( $WON_COUNT+1  ))
-elif [ $checkPosition -eq 1 ] && [ $START_POSITION -ge 0 ]
-then
-   	START_POSITION=$(( $START_POSITION - $rollDice ))
+	 		START_POSITION=$(( $START_POSITION + $rollDice ))
+			WON_COUNT=$(( $WON_COUNT+1  ))
+			if	[[	$START_POSITION -gt $END_POSITION ]]
+			then
+			START_POSITION=$(( $START_POSITION - $rollDice	))
+			fi
 
-dieChart["Position"$checkPosition]=$(( $START_POSITION ))
- if [[ $START_POSITION -le 0 ]]
- then
- START_POSITION=0
- fi 
-else
-   	START_POSITION=$START_POSITION
-fi
+		elif [ $checkPosition -eq 1 ] && [ $START_POSITION -ge 0 ]
+		then
+   		START_POSITION=$(( $START_POSITION - $rollDice ))
+
+			dieChart["Position"$checkPosition]=$(( $START_POSITION ))
+ 			if [[ $START_POSITION -le 0 ]]
+ 			then
+ 				START_POSITION=0
+ 			fi
+		else
+   		START_POSITION=$START_POSITION
+
+		fi
+
+  		 playersChart[ $i ]=$WON_COUNT
+	done
+
+ echo  ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#PLAYER=$(( $PLAYER-1  ))
 done
-
 }
+
 dice
+
 echo ${dieChart[@]} 
 echo $WON_COUNT
+echo	">>>>>>>>>>>>>>>>>>>>>> " ${!playersChart[@]}
+echo ${playersChart[@]}
+luckiest=$( printf "%s\n" ${gamblerSimulator[@]} | sort -nr | head -1 )
+
